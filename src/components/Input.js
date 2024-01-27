7cc7948371d63689fbd0ad6f8816a637c519711f
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-01-27 15:28:56 -0500

    css stuff

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,12 +6,26 @@ export default function Input() {
   const [typed_message, setMessage] = useState("");
   const handleChange = (event) => {
     setMessage(event.target);
-  }
+  };
   
-  return <input 
+  const handleFormSubmit = (event) => {
+    let submit_data = event.target.value;
+    
+    // TODO
+  };
+
+  return  <form>
+  <input
     type="text"
     id = "chatbot-message-input"
     className="input"
     onChange={handleChange}
   />
+
+  <input 
+    type="submit" 
+    value=">"
+  />
+
+  </form>
 }
