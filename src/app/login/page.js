4ea656cafd402c60eb3eab67a708991a9dea86dc
Justin Yang <111909590+JustinYang3@users.noreply.github.com>
2024-01-27 18:08:48 -0500

    login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { auth } from 'utils/config';
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { auth } from "utils/config";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const router = useRouter();
 
@@ -19,7 +19,7 @@ export default function Login() {
   const handleSignin = async () => {
     await signInWithEmailAndPassword(email, password);
     if (user) {
-      router.push('/');
+      router.push("/");
     } else {
       console.log(error);
     }
@@ -33,15 +33,27 @@ export default function Login() {
           type="text"
           placeholder="Email"
           value={email}
+          className={styles.text}
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
+          className={styles.text}
           onChange={(e) => setPassword(e.target.value)}
         />
         <button onClick={handleSignin}>Log in</button>
+        <input
+          type="checkbox"
+          id="remember"
+          name="rembember"
+          className={styles.check}
+        />
+        <label for="remember" className={styles.check}>
+          {" "}
+          Remember me
+        </label>
       </div>
     </div>
   );
