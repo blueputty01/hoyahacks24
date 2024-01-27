973566cd2e756eaf29eaa48988906d7db6313241
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-01-27 12:37:19 -0500

    basic ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,41 @@
+// import React, { useState } from 'react';
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import Input from '@/components/Input'
+import { useState } from "react";
+
+const messages = 
+[
+  {
+    id: 0,
+    content: "Welcome to SecurePod ! this is a lot of extra text that isn't necessary but hopefully causes a line overflow. A:WFLKJ ASDLFKJ WEPOFIJE WFPOIEWFKJ WES?LFKJ A:LKFJ EWPFOIE JFas"
+  },
+  {
+    id: 1,
+    content: "Welcome to SecurePod" 
+  }
+]
+
 export default function Home() {
+    
+
+  const messages_html = messages.map((message, idx) => {
+    return( <div key={message.id} className={message.id === 0 ? "message bot" : "message user"}>{message.content}</div> );
+  });
+  
   return (
     <main className={styles.main}>
+      <div><h1>Home</h1></div>
+
+      <div className="chatbox">
+        <div className="scrollbox">
+          {messages_html}
+        </div>
+        <Input/>
+      </div>
+
+
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
