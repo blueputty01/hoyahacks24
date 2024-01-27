561c7834be8a9955220f6f57fea1881c8fa47ea9
Ji19283756 <ji19283756@gmail.com>
2024-01-27 15:43:44 -0500

    edited files

--- src/app/api/llm/route.ts
+++ src/app/api/llm/route.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { pull, push } from "utils/db";
 
 export interface Env {
   AI: any;
 }
 
-async function run(model, input) {
+async function run(model: string, input: any) {
   if (!process.env.CF_TOKEN) {
     throw new Error('Missing CF_TOKEN');
   }
@@ -25,10 +26,16 @@ async function run(model, input) {
 
 export async function POST(req: NextRequest) {
   const request = JSON.parse(await req.text()) as any;
-  const { messages } = request;
+  const { message, user_id } = request;
 
-  const result = await run('@cf/meta/llama-2-7b-chat-int8', {
-    messages: messages,
+  let previous_questions = await pull(user_id) as string[];
+  const prev_messages = previous_questions.map((m) => ({
+    role: "user",
+    content: m,
+  }))
+
+  const result = await run('@cf/meta/llama-2-7b-chat-fp16', {
+    messages: [{role: "system", content: "You are SecurePod, a helpful and friendly chat bot that provides advice to the elderly on how to be safe on the Internet. You respond in a short, succinct, and understandable way. Please keep your responses less than 50 words."}, ...prev_messages, {role: "user", content: message}],
   });
 
   return NextResponse.json(result);
---
+++ src/utils/db.ts
@@ -0,0 +1,80 @@
+import { MongoClient } from 'mongodb';
+import { NextRequest, NextResponse } from 'next/server';
+
+export interface Env {
+  AI: any;
+}
+
+// export async function GET(req: NextRequest) {
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = 'mongodb+srv://ji19283756:16Bp00UEYrv7A7vCANx9@cluster0.3ztx48o.mongodb.net/?retryWrites=true&w=majority';
+
+// Create a new client and connect to MongoDB
+const client = new MongoClient(uri);
+
+export async function push(userId_: string, message_: string, response_: string) {
+  try {
+    // Connect to the "insertDB" database and access its "haiku" collection
+    const database = client.db('cluster0');
+    const people = database.collection('people');
+
+    // Create a document to insert
+    const doc = {
+      userId: userId_,
+      message: message_,
+      response: response_
+    };
+    // Insert the defined document into the "haiku" collection
+    const result = await people.insertOne(doc);
+
+    // Print the ID of the inserted document
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+    // Close the MongoDB client connection
+    await client.close();
+  }
+}
+  
+export async function pull(user_id: string) {
+  try {
+    // Connect to the "insertDB" database and access its "haiku" collection
+    const database = client.db('cluster0');
+    const people = database.collection('people');
+
+    // Query for movies that have a runtime less than 15 minutes
+    const query = { userId: { $eq: user_id } };
+
+    var questions = [];
+
+    const options = {
+      // Include only the `title` and `imdb` fields in each returned document
+      projection: { _id: 0, userId: 1, message: 1 },
+    };
+
+    // Execute query 
+    const cursor = people.find(query, options);
+
+    // Print a message if no documents were found
+    if ((await people.countDocuments(query)) === 0) {
+      console.log("No documents found!");
+    }
+
+    // Print returned documents
+    for await (const doc of cursor) {
+      questions.push(doc.message);
+    }
+  } finally {
+    // Close the MongoDB client connection
+    await client.close();
+    console.dir(questions);
+    return questions;
+  }
+}
+
+  // push("0", "what improves security in general?").catch(console.dir);
+  // pull("0").catch(console.dir); 
+
+  // return NextResponse.json({
+  //   message: 'Hello from the API!',
+  // });
+// }
