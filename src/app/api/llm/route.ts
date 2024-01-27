9021a5f5c89d1d4140a25cacb610c72fa1c5fe9a
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-01-27 11:35:32 -0500

    added message passing

--- src/app/api/llm/route.ts
+++ src/app/api/llm/route.ts
@@ -5,7 +5,9 @@ export interface Env {
 }
 
 async function run(model, input) {
-  console.log(process.env.CF_TOKEN);
+  if (!process.env.CF_TOKEN) {
+    throw new Error('Missing CF_TOKEN');
+  }
 
   const response = await fetch(
     `https://api.cloudflare.com/client/v4/accounts/9dcbe3f76f2d54534c6aba213b8e7a81/ai/run/${model}`,
@@ -20,20 +22,11 @@ async function run(model, input) {
 }
 
 export async function POST(req: NextRequest) {
-  console.log('hi');
+  const request = JSON.parse(await req.text()) as any;
+  const { messages } = request;
 
   const result = await run('@cf/meta/llama-2-7b-chat-int8', {
-    messages: [
-      {
-        role: 'system',
-        content: 'You are a friendly assistant that helps write stories',
-      },
-      {
-        role: 'user',
-        content:
-          'Write a short story about a llama that goes on a journey to find an orange cloud',
-      },
-    ],
+    messages: messages,
   });
 
   return NextResponse.json(result);
