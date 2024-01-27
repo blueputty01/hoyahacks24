24c6c8c2598c6083c19b03b39c8c5753df86974e
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-01-27 14:54:18 -0500

    fix jsconfig issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,42 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
-import Input from 'components/Input'
+import Input from 'components/Input';
 
-const messages = 
-[
+const messages = [
   {
     id: 0,
-    content: "Welcome to SecurePod! this is a lot of extra text that isn't necessary but hopefully causes a line overflow. A:WFLKJ ASDLFKJ WEPOFIJE WFPOIEWFKJ WES?LFKJ A:LKFJ EWPFOIE JFas"
+    content:
+      "Welcome to SecurePod! this is a lot of extra text that isn't necessary but hopefully causes a line overflow. A:WFLKJ ASDLFKJ WEPOFIJE WFPOIEWFKJ WES?LFKJ A:LKFJ EWPFOIE JFas",
   },
   {
     id: 1,
-    content: "Hello" 
-  }
-]
+    content: 'Hello',
+  },
+];
 
 export default function Home() {
   const messages_html = messages.map((message, idx) => {
-    return( <div key={message.id} className={message.id === 0 ? "message bot" : "message user"}>{message.content}</div> );
+    return (
+      <div
+        key={message.id}
+        className={message.id === 0 ? 'message bot' : 'message user'}
+      >
+        {message.content}
+      </div>
+    );
   });
-  
+
   return (
     <main className={styles.main}>
-      <div><h1>Home</h1></div>
-
-      <div className="chatbox">
-        <div className="scrollbox">
-          {messages_html}
-        </div>
-        {/* <Input
-          onChange={handleChange}
-        /> */}
+      <div>
+        <h1>Home</h1>
       </div>
 
+      <div className="chatbox">
+        <div className="scrollbox">{messages_html}</div>
+        <Input />
+      </div>
 
       <div className={styles.grid}>
         <a
