ebfc969c19f34d77650833f0bedcf969dea4fb02
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-01-27 21:07:17 -0500

    client

--- src/app/api/llm/route.ts
+++ src/app/api/llm/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { pull, push } from "utils/db";
+import { pull, push } from 'utils/db';
 
 export interface Env {
   AI: any;
@@ -10,7 +10,7 @@ async function run(model: string, input: any) {
     throw new Error('Missing CF_TOKEN');
   }
 
-  console.log(process.env.CF_TOKEN)
+  console.log(process.env.CF_TOKEN);
 
   const response = await fetch(
     `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT}/ai/run/${model}`,
@@ -28,15 +28,25 @@ export async function POST(req: NextRequest) {
   const request = JSON.parse(await req.text()) as any;
   const { message, user_id } = request;
 
-  let previous_questions = await pull(user_id) as string[];
+  let previous_questions = (await pull(user_id)) as string[];
   const prev_messages = previous_questions.map((m) => ({
-    role: "user",
+    role: 'user',
     content: m,
-  }))
+  }));
 
   const result = await run('@cf/meta/llama-2-7b-chat-fp16', {
-    messages: [{role: "system", content: "You are SecurePod, a helpful and friendly chat bot that provides advice to the elderly on how to be safe on the Internet. You respond in a short, succinct, and understandable way. Please keep your responses less than 50 words."}, ...prev_messages, {role: "user", content: message}],
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You are SecurePod, a helpful and friendly chat bot that provides advice to the elderly on how to be safe on the Internet. You respond in a short, succinct, and understandable way. Please keep your responses less than 50 words.',
+      },
+      ...prev_messages,
+      { role: 'user', content: message },
+    ],
   });
+
+  await push(user_id, message, result.response);
 
   return NextResponse.json(result);
 }
